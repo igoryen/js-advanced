b227461e47d80b61f7ab7515d894490a2a337176
igoryen <igor.yentaltsev@gmail.com>
2021-11-09 19:14:19 -0500

    hidden classes

--- 014-writing-optimized-code.js
+++ 014-writing-optimized-code.js
@@ -1,3 +1,28 @@
+// hidden classes
+function Animal(x,y) { // 3
+    this.x = x;
+    this.y = y;
+}
+
+const obj1 = new Animal(1,2);
+const obj2 = new Animal(3,4);
+
+obj1.a = 30;
+obj1.b = 100;
+
+// 2
+obj2.b = 30;
+obj2.a = 100;
+
+// 2: these 2 lines will deoptimize the code
+// i.e. make the compiler run slower
+// Why? Because the order of assignment is not as before
+// not obj2.a and then obj2.b
+// but obj2.b and then obj2.a
+// The assignment order should go as in #3
+
+
+
 // inline caching
 
 function findUser(user) {
