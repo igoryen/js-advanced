6de45fcef5ef410c71b7025322d1e2d15c554937
igoryen <igor.yentaltsev@gmail.com>
2021-11-09 19:20:53 -0500

    memory heap

---
+++ 016 - call-stack-mem-heap.js
@@ -0,0 +1,8 @@
+const number = 610;
+const string = '610'; // string six one zero
+const human = {
+    first: 'Igor',
+    last: 'Entaltsev'
+}
+
+// number string human - will get mem space allocated by engine
