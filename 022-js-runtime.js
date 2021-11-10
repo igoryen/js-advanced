9a01c661f761df99979c80719f6ac2c0c4241a7a
igoryen <igor.yentaltsev@gmail.com>
2021-11-09 19:52:25 -0500

    go to web api 3

--- 022-js-runtime.js
+++ 022-js-runtime.js
@@ -1,7 +1,7 @@
 // 022-js-runtime.js
 
 console.log('1');
-setTimeout( () => {console.log('2'), 1000});
+setTimeout( () => {console.log('2'), 0});
 console.log('3');
 
-// the result is: 1,3,2
+// the result is: 1,3,2 anyway
