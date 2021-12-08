cbe0fac5ab6d6cdddae3949a3eed3812a04feaaf
igoryen <igor.yentaltsev@gmail.com>
2021-12-08 17:00:01 -0500

    033 hoisting exe b

--- 033-hoisting-exe.js
+++ 033-hoisting-exe.js
@@ -1,9 +1,23 @@
-var one = 1;
-var one = 2;
+a();
+
+function a() {
+    console.log('hi');
+}
+
+function a() {
+    console.log('bye');
+}
 
 /**
- * creation phase:
- * JS engine sees var, and allocates memory for variable 'one', its value is 'undefined'.
- * Then JS sees var, and then 'one' again...
- * JS engine IGNORES the second 'one'.
+ * Creation phase:
+ * The JS engine comes to Line 3,
+ * sees 'function', allocates memory for the whole f-object.
+ * At line 7, it sees 'function' with the same name,
+ * allocates memory/overrides what was there before.
+ * Execution phase:
+ * At line 1,
+ * the f-object is invoked,
+ * using the data allocated in memory,
+ * returning 'bye'
+ *
  */
