0f7a5c4b5f97b6cc5332e121d53feebde7ef464f
igoryen <igor.yentaltsev@gmail.com>
2021-12-08 18:06:35 -0500

    034 hoisting exe 2b

--- 034-hoisting-exe-2.js
+++ 034-hoisting-exe-2.js
@@ -1,9 +1,9 @@
-var faveFood = "grapes";
+const faveFood = "grapes";
 
-var foodThoughts = function () {
-    console.log("Original fave food: " + faveFood);
+const foodThoughts = function () {
+    console.log("Original fave food: " + faveFood); // 1
 
-    var faveFood = "lobster";
+    const faveFood = "lobster";
 
     console.log("New fave food: " + faveFood);
 }
@@ -11,30 +11,9 @@ var foodThoughts = function () {
 foodThoughts();
 
 /**
- * cs == call stack
- *
- * cs[0] = global environment
- *
- * cs[0].creationPhase starts.
- * line 1: var! cs[0].faveFood = undefined.
- * line 3: var! cs[0].foodThoughts = undefined.
- *
- * cs[0].executionPhase:
- * line 1: cs[0].faveFood = "grapes"
- * line 2: cs[0].foodThoughts = f-object.
- * line 11: run cs[0].foodThoughts()
- *
- * cs[1] = foodThoughts environment
- * cs[1].creationPhase starts.
- * line6: var! cs[1].faveFood: undefined.
- *
- * cs[1].executionPhase:
- * line 4: output "Original fave food: undefined. // cs[1].faveFood
- * line 6: cs[1].faveFood = "lobster"
- * line 8: output "New fave food: lobster // cs[1].faveFood
- *
- * cs[1] clears.
- * cs[0] clears.
- *
- *
+ * 1. ReferenceError: favouriteFood is not defined.
+ * Because
+ * Andre says: with let and const, no hoisting happens.
+ * Does that mean that no Creation phase happens???
+ * Or it does happen, but it ends quickly, because no memory allocation happens.
  */
