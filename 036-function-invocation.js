cd7bc365aa79968d879c22c1657c194c50e175cf
igoryen <igor.yentaltsev@gmail.com>
2021-12-08 18:21:52 -0500

    036 function invocation a

---
+++ 036-function-invocation.js
@@ -0,0 +1,17 @@
+// function expression
+var canada = function () {
+    console.log('cold')
+}
+
+
+// function declaration
+function india() {
+    console.log('warm')
+}
+
+/**
+ * function expressions do not get hoisted
+ * (do not get memorized)
+ *
+ * but function declarations do.
+ */
