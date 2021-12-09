04d7f10be1ef01d3fdcc7a236e65daa246a1fb4f
igoryen <igor.yentaltsev@gmail.com>
2021-12-08 23:48:32 -0500

    037 arguments keyword a

--- 036-function-invocation.js
+++ 036-function-invocation.js
@@ -14,15 +14,24 @@ canada()
 india()
 
 function marry(person1, person2) {
-    console.log(arguments);
+    console.log("m1", arguments);
+    console.log("m1", Array.from(arguments)); // 1
     return `${person1} is now married to ${person2}`
 }
 
-marry('Tim', 'Tina');
+function marry2(...args) {
+    console.log("m2", args);
+    console.log("m2", Array.from(arguments)); // 1
+    return `${args[0]} is now married to ${args[0]}`
+}
+
+marry2('Tim', 'Tina');
 
 /**
  * function expressions do not get hoisted
  * (do not get memorized)
  *
  * but function declarations do.
+ *
+ * 1. [ 'Tim', 'Tina' ]
  */
