ac8487b0d38a1c2b3852f963a30ed29e3a88d754
igoryen <igor.yentaltsev@gmail.com>
2021-12-09 19:24:40 -0500

    039 - scope chain B

--- 039-scope-chain.js
+++ 039-scope-chain.js
@@ -1,22 +1,67 @@
-var x = 'x';
-
-function findName() {
-    console.log(x);
-    var b = 'b';
-    return printName();
-}
-
-function printName() {
-    var c = 'c';
-    return 'igoryen';
-}
-
 function sayMyName() {
+    debugger;
     var a = 'a';
-    return findName();
+    return function findName() {
+        var b = 'b';
+        return function printName() {
+            var c = 'c';
+            return 'igoryen';
+        }
+    }
 }
 
-sayMyName();
+sayMyName()()();
+
+/**
+ * cs[0] CREATION/PARSING
+ * cs[0].sayMyName = f-object
+ * cs[0] EXECUTION
+ * L12: sayMyName()
+ *
+ * cs[1] = sayMyName() environment
+ * cs[1] CREATION
+ * cs[1].a = 'undefined'
+ * cs[1] EXECUTION
+ * L2: cs[1].a = 'undefined'
+ * L3: return text of findName()
+ * [???] add '()' and run
+ *
+ * cs[2] = findName() environment
+ * cs[2] CREATION
+ * cs[2].b = 'undefined'
+ * cs[2] EXECUTION
+ * L4: cs[2].b = 'b'
+ * L5: printName()
+ * [???] add '()' and run
+ *
+ * cs[3] = printName() environment
+ * cs[3] CREATION
+ * cs[3].c = 'undefined'
+ * cs[3] EXECUTION
+ * L6: cs[3].c = 'c'
+ *
+ */
+
+//====================
+// var x = 'x';
+
+// function findName() {
+//     console.log(x);
+//     var b = 'b';
+//     return printName();
+// }
+
+// function printName() {
+//     var c = 'c';
+//     return 'igoryen';
+// }
+
+// function sayMyName() {
+//     var a = 'a';
+//     return findName();
+// }
+
+// sayMyName();
 
 /**
  * cs[0] CREATION/PARSING
@@ -24,31 +69,31 @@ sayMyName();
  * cs[0].findName: all f-object
  * cs[0].sayMyName: all f-object
  * cs[0] EXECUTION
- * line 1: cs[0].x = 'x'
- * line 19: sayMyName()
+ * L1: cs[0].x = 'x'
+ * L19: sayMyName()
  *
  * cs[1] = sayMyName() environment
  * cs[1] CREATION/PARSING
  * cs[1].a = 'undefined'
  * cs[1] EXECUTION
- * line 16: findName()
+ * L16: findName()
  *
  * cs[2] = findName() env
  * cs[2] CREATION/PARSING
  * cs[2].b = 'undefined'
  * cs[2] EXECUTION
- * line 4: search for cs[2].x: not found
+ * L4: search for cs[2].x: not found
  *         search for cs[1].x: not found
  *         search for cs[0].x: found 'x' // --> x
- * line 5: cs[2].b = 'b'
- * line 6: printName()
+ * L5: cs[2].b = 'b'
+ * L6: printName()
  *
  * cs[3] printName() environment
  * cs[3] CREATION/PARSING
  * cs[3].c = 'undefined'
  * cs[3] EXECUTION
- * line 10: cs[3].c = 'c'
- * line 11: // --> igoryen
+ * L10: cs[3].c = 'c'
+ * L11: // --> igoryen
  *
  * cs[3] clears
  * cs[2] clears
