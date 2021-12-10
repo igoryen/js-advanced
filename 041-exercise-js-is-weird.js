93d58d23f27b0f54bdfba85966ce754cb60e5603
igoryen <igor.yentaltsev@gmail.com>
2021-12-09 20:42:01 -0500

    041 exercise C

--- 041-exercise-js-is-weird.js
+++ 041-exercise-js-is-weird.js
@@ -1,10 +1,34 @@
-'use strict'
-
-function weird() {
-    height = 50;
-    return height;
+var heyhey = function doodle() {
+    return 'heyhey';
 }
-weird();
+
+heyhey();
+doodle();
+
+/**
+ * cs[0] CREATION
+ * L1: cs[0].heyhey = f-object whole
+ * cs[0] EXECUTION
+ * L5: run cs[0].heyhey()
+ *
+ * cs[1] heyhey() environment
+ * cs[1] CREATION
+ * nothing to do
+ * cs[1] EXECUTION
+ * L2: return 'heyhey' // heyhey
+ * cs[1] clears
+ *
+ * cs[0].doodle // ReferenceError: doodle is not defined
+ */
+
+//===================================
+// 'use strict'
+
+// function weird() {
+//     height = 50;
+//     return height;
+// }
+// weird();
 
 /**
  * cs[0] CREATION
@@ -15,12 +39,5 @@ weird();
  * cs[1] = weird() environment
  * cs[1] CREATION
  * L2: 'height'??? --> ReferenceError: height is not defined
- * cs[0].height = 'undefined'
- * cs[1] EXECUTION
- * cs[0].height = 50
- * L3: cs[1].height: not found
- *     cs[0].height: 50 // --> 50
- * cs[1] clears
- * cs[0] clears
  *
  */
